af1e2b5ff1f744aac605effcc45f8006b76f64d0
505 <505@academicos.cesde.edu.co>
2022-08-31 20:50:42 -0500

    se crea galeria de pelicula clase 31-08-2022

--- src/controllers/controladorCINEMA.js
+++ src/controllers/controladorCINEMA.js
@@ -125,6 +125,8 @@ let peliculas=[
 ]
 
 //RECORRIENDO UN ARREGLO EN JAVASCRIPT
+let fila=document.getElementById("fila")
+
 peliculas.forEach(function(pelicula){
     //console.log(pelicula);
     console.log(pelicula.nombre)
@@ -134,4 +136,63 @@ peliculas.forEach(function(pelicula){
     console.log(pelicula.sinopsis)
     console.log(pelicula.clasificacion)
     console.log(pelicula.idioma)
+/*
+    //TRAVERSING (CREAR ETIQUETAS DE HTML DESDE JS)
+    let poster=document.createElement("img")
+    poster.src=pelicula.poster
+    let nombrePelicula=document.createElement("h2")
+    nombrePelicula.textContent=pelicula.nombre
+    //PADRES E HIJOS
+    fila.appendChild(poster)
+    fila.appendChild(nombrePelicula)
+    */
+
+    //TRAVERSING (CREAR ETIQUETAS DE HTML DESDE JS)
+
+    //1. CREAMOS UNA COLUMNA PARA CADA PELICULA
+    let columna=document.createElement("div")
+    columna.classList.add("col")
+
+    //2. creamos una terjeta para cada pelicula
+    let tarjeta=document.createElement("div")
+    tarjeta.classList.add("card","h-100")
+
+    //3. creamos una foto para cada pelicula
+    let poster=document.createElement("img")
+    poster.classList.add("card-img-top")
+    poster.src=pelicula.poster
+
+    // 4. crear nombre de la pelicula
+    let nombre=document.createElement("h3")
+    nombre.classList.add("card-title","text-center")
+    nombre.textContent=pelicula.nombre
+    // 4. creamos el genero de cada pelicula
+    let genero=document.createElement("h4")
+    genero.classList.add("text-start")
+    genero.textContent="Genero: "+pelicula.genero
+
+    //6. creamos el idioma de cada pelicula
+    let idioma=document.createElement("h6")
+    idioma.classList.add("fw-bold")
+    idioma.textContent="Idioma: "+pelicula.idioma
+
+    //7. creamos la sinopsis
+    let sinopsis=document.createElement("p")
+    sinopsis.classList.add("d-none")
+    sinopsis.textContent=pelicula.sinopsis
+
+    //PADRES E HIJOS
+    tarjeta.appendChild(poster)
+    tarjeta.appendChild(nombre)
+    tarjeta.appendChild(genero)
+    tarjeta.appendChild(idioma)
+    tarjeta.appendChild(sinopsis)
+    columna.appendChild(tarjeta)
+    fila.appendChild(columna)
+
+})
+
+//detectando seleccion de una pelicula
+fila.addEventListener("click",function(){
+    alert("esta seleccionando una pelicula")
 })
