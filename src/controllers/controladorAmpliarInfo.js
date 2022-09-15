4773acc81f6bb77f7ab14f28f725c3dfba8b6fd8
505 <505@academicos.cesde.edu.co>
2022-09-14 20:49:14 -0500

    clase 14-09-2022 eventos

---
+++ src/controllers/controladorAmpliarInfo.js
@@ -0,0 +1,4 @@
+//PREGUNTANDO POR UN DATO QUE ESTA ALMACENADO EN MEMORIA
+
+let reseptora=localStorage.getItem("peliculaSeleccionada")
+console.log(reseptora)
---
+++ src/helper/baseDatosNuevas.js
@@ -0,0 +1,54 @@
+export let peliculasestreno=[
+    {
+        nombre:"Apocalipsis zombie",
+        genero:"Ciencia ficcion",
+        duracion:149,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cineca-e943f.appspot.com/o/ApocalipsisZombie.png?alt=media&token=64ddb842-f251-4ba0-9821-9887ad093d50",
+        sinopsis:"Cuenta la historia de Kya, una niña abandonada que se crió hasta la edad adulta en los peligrosos pantanos de Carolina del Norte. Durante años, los rumores sobre la",
+        clasificacion:"General",
+        idioma:"En subtitulado",
+        director:"Steven Spielberg",
+        actores:["Bruce Willis","Johnny Depp","Jason Statham"]
+    },
+    {
+        nombre:"Caceria de brujas",
+        genero:"Terror",
+        duracion:149,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cineca-e943f.appspot.com/o/CaceriaDeBrujas.png?alt=media&token=0029fd90-7f8d-4666-9713-f7450bc8048b",
+        sinopsis:"Cuenta la historia de Kya, una niña abandonada que se crió hasta la edad adulta en los peligrosos pantanos de Carolina del Norte. Durante años, los rumores sobre la",
+        clasificacion:"General",
+        idioma:"En subtitulado",
+        director:"Steven Spielberg",
+        actores:["Bruce Willis","Johnny Depp","Jason Statham"]
+    },{
+        nombre:"El arbol rojo",
+        genero:"Ciencia ficcion",
+        duracion:149,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cineca-e943f.appspot.com/o/trenbala.jpg?alt=media&token=91e29996-2063-4202-b09b-ed4ba15066df",
+        sinopsis:"lCuenta la historia de Kya, una niña abandonada que se crió hasta la edad adulta en los peligrosos pantanos de Carolina del Norte. Durante años, los rumores sobre la",
+        clasificacion:"General",
+        idioma:"En subtitulado",
+        director:"Steven Spielberg",
+        actores:["Bruce Willis","Johnny Depp","Jason Statham"]
+    },{
+        nombre:"La chica salvaje",
+        genero:"Ciencia ficcion",
+        duracion:149,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cineca-e943f.appspot.com/o/ElArbolRojo.png?alt=media&token=1dc526b9-5df5-4e98-b046-f3c77bb5b76c",
+        sinopsis:"Cuenta la historia de Kya, una niña abandonada que se crió hasta la edad adulta en los peligrosos pantanos de Carolina del Norte. Durante años, los rumores sobre la",
+        clasificacion:"General",
+        idioma:"En subtitulado",
+        director:"Steven Spielberg",
+        actores:["Bruce Willis","Johnny Depp","Jason Statham"]
+    },{
+        nombre:"Lo que arde",
+        genero:"Ciencia ficcion",
+        duracion:149,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cineca-e943f.appspot.com/o/LoQueArde.png?alt=media&token=b94f3de2-6e53-4d93-a060-b988bcd367dc",
+        sinopsis:"Cuenta la historia de Kya, una niña abandonada que se crió hasta la edad adulta en los peligrosos pantanos de Carolina del Norte. Durante años, los rumores sobre la",
+        clasificacion:"General",
+        idioma:"En subtitulado",
+        director:"Steven Spielberg",
+        actores:["Bruce Willis","Johnny Depp","Jason Statham"]
+    }
+]
